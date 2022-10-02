06e1d60f6a8f06e533f8f70e9a1dae35171f3f56
Tharindu-Nirmal <tharindu.ncr@gmail.com>
2022-10-02 17:56:47 +0530

    first20worksCompression_line6

--- maincode.cpp
+++ maincode.cpp
@@ -167,7 +167,7 @@ while (!stringque.empty()){
 
 //copy data strings to a vector of vector<int>, intialised into vectors of zeros
 // int N_strings = datastrings.size();
-int N_strings = 14;
+int N_strings = 21;
 
 std::vector<std::vector<int>> datavector_2d(N_strings, std::vector<int> (32, 0));
 for (int i = 0; i<N_strings; i++){
