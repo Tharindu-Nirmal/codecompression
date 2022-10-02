2e5e21e8de5be6b8bf5912e2a3970038614886c3
Tharindu-Nirmal <tharindu.ncr@gmail.com>
2022-10-03 04:16:41 +0530

    case0and1are painn

--- decompressmain.cpp
+++ decompressmain.cpp
@@ -38,6 +38,7 @@ auto decToBinary(int n, int bitsize){
 }
 
 int binaryToDec(std::vector<int> data, int start, int width){
+    //convert binary repr in a vector to decimal
     int result = 0;
     for(int i=0;i<width;i++){ result = result + data[start+i]*pow(2, width-i-1);}
     return result;
@@ -67,8 +68,92 @@ int main(){
         stringque.pop();
     }
 
+    //containers for result
+    std::vector<std::vector<int>> decompout;
+
+    //outputfile
+    std::ofstream decompfile("dout.txt");
+
+    //loop through the digits
+    int current_index = 0; //of the data vector
+    int current_line =0;
+    std::vector <int> justdecoded;
+
+    while (current_index<< (numlines*32)){
+        int compressioncode = binaryToDec(compressedbits,current_index,3);
+        switch(compressioncode){
+            case 0:
+                int repition = 1+binaryToDec(compressedbits,current_index+3,2);
+                //deal with repitition.......
+                current_index = current_index + 10;
+                break;
+
+            case 1:
+                //masking
+                int firstmm = binaryToDec(compressedbits,current_index+3,5);
+                int baseindex = binaryToDec(compressedbits,current_index+13,3);
+                //apply bitmask.......
+                current_index = current_index + 15;
+                break;
+
+            case 2:
+                //1 bit MM 
+                int firstmm = binaryToDec(compressedbits,current_index+3,5);
+                int baseindex = binaryToDec(compressedbits,current_index+13,3);
+                for(int i=0;i<32;i++){
+                    if (i==firstmm){decompfile<< (basisvector_2d[baseindex][i] + 1)%2;justdecoded.push_back((basisvector_2d[baseindex][i] + 1)%2);} //flip bit
+                    else{decompfile<<basisvector_2d[baseindex][i];justdecoded.push_back(basisvector_2d[baseindex][i]);}
+                current_index = current_index + 11;
+                break;
+
+            case 3:
+                //2 bit MM consec
+                int firstmm = binaryToDec(compressedbits,current_index+3,5);
+                int baseindex = binaryToDec(compressedbits,current_index+13,3);
+                for(int i=0;i<32;i++){
+                    if ((i==firstmm) || (i==firstmm+1)){decompfile<< (basisvector_2d[baseindex][i] + 1)%2;justdecoded.push_back((basisvector_2d[baseindex][i] + 1)%2);} //flip bit
+                    else{decompfile<<basisvector_2d[baseindex][i];justdecoded.push_back(basisvector_2d[baseindex][i]);}
+                }
+                current_index = current_index + 11;
+                break;
+
+            case 4:
+                //2 bit MM far
+                int firstmm = binaryToDec(compressedbits,current_index+3,5);
+                int secondmm = binaryToDec(compressedbits,current_index+8,5);
+                int baseindex = binaryToDec(compressedbits,current_index+13,3);
+                for(int i=0;i<32;i++){
+                    if ((i==firstmm) || (i==secondmm)){decompfile<< (basisvector_2d[baseindex][i] + 1)%2;
+                    justdecoded.push_back((basisvector_2d[baseindex][i] + 1)%2);}
+                     //flip bit
+                    else{decompfile<<basisvector_2d[baseindex][i];justdecoded.push_back(basisvector_2d[baseindex][i]);}
+                }
+                current_index = current_index + 16;
+                break;
+
+            case 5:
+                //Directmatching
+                int baseindex = binaryToDec(compressedbits,current_index+3,3);
+                for(int i=0;i<32;i++){decompfile<<basisvector_2d[baseindex][i]; justdecoded.push_back(basisvector_2d[baseindex][i]);}
+                current_index = current_index + 6;
+                break;
+
+            case 6:
+                //Original Binary
+                for(int i=0;i<32;i++){decompfile<<compressedbits[current_index+3+i];justdecoded.push_back(compressedbits[current_index+3+i]);}
+                current_index = current_index + 35;
+                break;
+
+            case 7:
+                //a set of ones..end
+                current_index = current_index+ 40;
+                break;
+        }
+
+        decompfile<<'\n';
+    }
     // tests
     // std::cout<< binaryToDec(basisvector_2d[4],1,3);
 
     return 0;
-}
+    }
